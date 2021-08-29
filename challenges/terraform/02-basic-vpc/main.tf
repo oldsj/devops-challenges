44fc7c5d22977de2cb972448fbe020ff600636d1
sanil tison <saniltison@gmail.com>
2021-08-29 16:31:28 -0400

    completed challenge 02
    completed challenge
    - got a SAT with the bats files

--- challenges/terraform/02-basic-vpc/main.tf
+++ challenges/terraform/02-basic-vpc/main.tf
@@ -1,1 +1,18 @@
 # your code here...
+
+
+resource "aws_vpc" "basic_vpc" {
+  cidr_block       = "172.16.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "basic_vpc"
+  }
+}
+
+
+
+
+output "vpc_id" {
+  value = aws_vpc.basic_vpc.id
+}
