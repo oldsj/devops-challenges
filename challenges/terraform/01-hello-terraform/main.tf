ddb90c632e412b945fe9e7963d5e9e4a7bd54849
sanil tison <saniltison@gmail.com>
2021-08-29 16:21:02 -0400

    initial commit
    Completed challenge 01-hello-terraform

--- challenges/terraform/01-hello-terraform/main.tf
+++ challenges/terraform/01-hello-terraform/main.tf
@@ -1,1 +1,24 @@
 # your code here
+
+
+resource "aws_instance" "tf_instance"{
+
+  ami                    = "ami-00399ec92321828f5"
+  # enter ami string from step-1
+
+  instance_type          = "t2.micro"
+  # enter instance type string from step-2
+
+  key_name               = "cloud-ubuntu"
+  # enter key pair name 
+
+  tags = {
+    Name   = "hello-terraform"
+
+  }
+}
+
+
+output "instance_id" {
+  value = aws_instance.tf_instance.tags
+}
